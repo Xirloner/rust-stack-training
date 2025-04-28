5ec6ce607fff81c0d1e727ea8dbce5060a1cf77b
Luis Parada <luisparada364@gmail.com>
2025-04-28 02:05:28 +0200

    Change structure of the app, add database logic, user migration, basic ci/cd, migrations

---
+++ api/src/main.rs
@@ -0,0 +1,71 @@
+mod database;
+mod models;
+mod state;
+use anyhow::Result;
+use axum::extract::State;
+use axum::{Router, response::Json, routing::get};
+use database::connect_to_database;
+use deepseek_api::{Client, request::MessageRequest};
+use dotenv::dotenv;
+use serde_json::{Value, json};
+use state::AppState;
+use std::sync::Arc;
+use std::vec;
+use tracing::error;
+
+#[tokio::main]
+async fn main() {
+    tracing_subscriber::fmt::init();
+    dotenv().ok();
+    let db = match connect_to_database().await {
+        Ok(db) => db,
+        Err(e) => {
+            error!("Failed to connect to database: {}", e);
+            std::process::exit(1); // Exit with error
+        }
+    };
+    // Build shared app state
+    let app_state = AppState::new(Arc::new(db));
+
+    // build our application with a single route
+    let app = Router::new()
+        .route("/", get(dummy_post))
+        .route("/home", get(home))
+        .with_state(app_state);
+
+    // run our app with hyper, listening globally on port 3000
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    axum::serve(listener, app).await.unwrap();
+}
+
+async fn query_ds() -> Result<()> {
+    let client = Client::new("");
+    println!("In query_ds");
+    let mut history = vec![];
+    let mut completions = client.chat();
+    let builder = completions
+        .chat_builder(vec![])
+        .append_user_message("Hi Deepseek!");
+    let resp = completions.create(builder).await?.must_response();
+
+    let mut resp_words = vec![];
+    for msg in resp.choices.iter() {
+        let resp_msg = MessageRequest::from_message(msg.message.as_ref().expect("message"))?;
+        history.push(resp_msg);
+        resp_words.push(msg.message.as_ref().expect("message").content.clone());
+    }
+    for msg in resp_words.iter() {
+        msg.split("\n").for_each(|x| println!("{}", x));
+    }
+    Ok(())
+}
+
+async fn dummy_post() -> Json<Value> {
+    let _ = query_ds().await;
+    Json(json!({ "response": 200 }))
+}
+
+async fn home(State(state): State<AppState>) -> Json<Value> {
+    let _db_connection = state.db_arc();
+    Json(json!({ "response": 200 }))
+}
---
+++ migration/src/m20250428_000001_create_user_table.rs
@@ -0,0 +1,74 @@
+use sea_orm_migration::prelude::*;
+
+#[derive(DeriveMigrationName)]
+pub struct Migration;
+
+#[async_trait::async_trait]
+impl MigrationTrait for Migration {
+    async fn up(&self, manager: &SchemaManager) -> Result<(), DbErr> {
+        manager
+            .create_table(
+                Table::create()
+                    .table(User::Table)
+                    .if_not_exists()
+                    .col(
+                        ColumnDef::new(User::Id)
+                            .integer()
+                            .not_null()
+                            .auto_increment()
+                            .primary_key(),
+                    )
+                    .col(ColumnDef::new(User::Email).string().not_null())
+                    .col(ColumnDef::new(User::HashedPassword).string().not_null())
+                    .col(
+                        ColumnDef::new(User::IsActive)
+                            .boolean()
+                            .not_null()
+                            .default(true),
+                    )
+                    .col(
+                        ColumnDef::new(User::IsEmailVerified)
+                            .boolean()
+                            .not_null()
+                            .default(false),
+                    )
+                    .col(
+                        ColumnDef::new(User::IsDeleted)
+                            .boolean()
+                            .not_null()
+                            .default(false),
+                    )
+                    .col(
+                        ColumnDef::new(User::CreatedAt)
+                            .timestamp_with_time_zone()
+                            .not_null(),
+                    )
+                    .col(
+                        ColumnDef::new(User::UpdatedAt)
+                            .timestamp_with_time_zone()
+                            .not_null(),
+                    )
+                    .to_owned(),
+            )
+            .await
+    }
+
+    async fn down(&self, manager: &SchemaManager) -> Result<(), DbErr> {
+        manager
+            .drop_table(Table::drop().table(User::Table).to_owned())
+            .await
+    }
+}
+
+#[derive(Iden)]
+enum User {
+    Table,
+    Id,
+    Email,
+    HashedPassword,
+    IsActive,
+    IsEmailVerified,
+    IsDeleted,
+    CreatedAt,
+    UpdatedAt,
+}
